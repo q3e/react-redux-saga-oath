8ee94063ec069fb0b669c844bb053b43bbae7044
Fatah N <nfatah41@gmail.com>
2018-09-26 11:25:11 +0300

    save session & track token expiry using momentjs

--- src/redux/Login/api.js
+++ src/redux/Login/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import moment from 'moment'
 
 const login = credentials => axios({
   url: 'https://devapi.careerprepped.com/oauth',
@@ -8,8 +9,9 @@ const login = credentials => axios({
     grant_type: 'password',
     client_id: 'careerprepped'
   }})
-  .then(function (response) {
-    console.log(response);
+  .then(function ({ data }) {
+    localStorage.setItem('access_token', data.access_token)
+    localStorage.setItem('token_expiry', moment.now() + data.expires_in)
   })
   .catch(e => Promise.reject(e));
 

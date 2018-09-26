574bd55f0e7d3187a71355b244e0697f9578911f
Fatah N <nfatah41@gmail.com>
2018-09-26 08:51:21 +0300

    remove unneeded console

--- src/redux/Login/sagas.js
+++ src/redux/Login/sagas.js
@@ -5,7 +5,6 @@ import loginAPI from './api'
 
 // main worker saga - fired on LOGIN_REQUESTED action
 function* login(action) {
-  console.log(_pick(action, ['email', 'password']))
    try {
       const user = yield call(loginAPI, _pick(action, ['email', 'password']))
       yield put({type: "LOGIN_SUCCEEDED", user: user})

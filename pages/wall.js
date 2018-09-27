f59982947b1b56f5576a945db2e300b4d3b1502a
Fatah N <nfatah41@gmail.com>
2018-09-27 19:14:23 +0300

    update wall page css import

--- pages/wall.js
+++ pages/wall.js
@@ -3,12 +3,11 @@ import { Container } from 'reactstrap'
 import Posts from 'src/containers/Posts'
 import { postsActionCreator as posts } from 'src/redux/Posts/actions'
 
-import '../src/assets/bootstrap.scss'
+import 'src/assets/bootstrap.scss'
 
 const Wall = () => pug`
   Container
     Posts
 `
-
 
 export default Wall

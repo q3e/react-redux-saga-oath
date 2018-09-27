670ad39f559c50e006558747b9ea0ca7aa0daaa7
Fatah N <nfatah41@gmail.com>
2018-09-27 05:37:29 +0300

    add bootstrap to wall page

--- pages/wall.js
+++ pages/wall.js
@@ -3,16 +3,12 @@ import { Container } from 'reactstrap'
 import Posts from 'src/containers/Posts'
 import { postsActionCreator as posts } from 'src/redux/Posts/actions'
 
+import '../src/assets/bootstrap.scss'
 
 const Wall = () => pug`
   Container
     Posts
 `
 
-// Wall.getInitialProps = async ctx => {
-//   if(!ctx.isServer){
-//     console.log(ctx)
-//     ctx.store.dispatch(posts(localStorage.getItem('access_token')))
-//   }
-// }
+
 export default Wall
